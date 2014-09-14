5091b4cf2450b431893e990f080797b5c448b408
Dmitriy Shekhovtsov <valorkin@gmail.com>
2014-09-14 13:43:43 +0300

    jshint happines

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('lint', function () {
   return gulp
-    .src(['/lib/*.js'])
+    .src(['lib/*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
 });
 
